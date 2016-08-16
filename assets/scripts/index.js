8d5b9beb44f84192bee04d02e0946e161f44a1b0
alex-braun <braunacb@gmail.com>
2016-08-16 16:40:04 -0400

    Added bundle for drumEvents to index.js.
    drumEvents and drumHandlers.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,4 +4,13 @@
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+
+// const drumEvents = require('./drumEvents');
+// const drumPatterns = require('./drumPatterns');
+// const knobJQuery = require('./jquery.knob.min');
+const drumEvents = require('./drumEvents');
+
+
+$(() => {
+  drumEvents.addDrumHandlers();
+});
