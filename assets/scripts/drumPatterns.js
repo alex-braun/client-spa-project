3bbf7d344f118df466110b903025e175dcedee68
alex-braun <braunacb@gmail.com>
2016-08-16 16:35:27 -0400

    Created constructPattern, userSelectDrum, padIndex
    Functions were developed earlier.  padIndex and userSelectDrum only
    take variables from handlers file.

--- assets/scripts/drumPatterns.js
+++ assets/scripts/drumPatterns.js
@@ -19,3 +19,88 @@ let hatClosePattern = ['','','','','','','',''];
 let hatOpenPattern = ['','','','','','','',''];
 let clapPattern = ['','','','','','','',''];
 let drum;
+
+let constructPattern = function(drum, index) {
+  // console.log(drum);
+  for (let i = 0; i < triggerGrid.length; i++) {
+    if (triggerGrid[i] === index && drum === 'kick') {
+      if (kickPattern[i] === '') {
+        kickPattern[i] = true;
+      } else {
+        kickPattern[i] = '';
+      }
+        console.log(kickPattern);
+        kick.get(0).play();
+    } else if (triggerGrid[i] === index && drum === 'snare') {
+      if (snarePattern[i] === '') {
+        snarePattern[i] = true;
+      } else {
+        snarePattern[i] = '';
+      }
+        console.log(snarePattern);
+        snare.get(0).play();
+    } else if (triggerGrid[i] === index && drum === 'hat-close') {
+      if (hatClosePattern[i] === '') {
+        hatClosePattern[i] = true;
+      } else {
+        hatClosePattern[i] = '';
+      }
+        console.log(hatClosePattern);
+        hatClose.get(0).play();
+    } else if (triggerGrid[i] === index && drum === 'hat-open') {
+      if (hatOpenPattern[i] === '') {
+        hatOpenPattern[i] = true;
+      } else {
+        hatOpenPattern[i] = '';
+      }
+        console.log(hatOpenPattern);
+        hatOpen.get(0).play();
+    } else if (triggerGrid[i] === index && drum === 'clap') {
+      if (clapPattern[i] === '') {
+        clapPattern[i] = true;
+      } else {
+        clapPattern[i] = '';
+      }
+        console.log(clapPattern);
+        clap.get(0).play();
+      }
+    }
+};
+
+
+let userSelectDrum = function(inst) {
+drum = inst;
+};
+
+let padIndex = function(grid) {
+  constructPattern(drum, grid);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  constructPattern,
+  padIndex,
+  userSelectDrum
+};
