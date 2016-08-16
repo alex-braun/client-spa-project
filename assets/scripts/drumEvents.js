3df0f4a5cd60ddb8d0037a454be0aef5548c974d
alex-braun <braunacb@gmail.com>
2016-08-16 16:28:16 -0400

    Added function for selecting drum type
    Handlers for figuring out which drum the user has selected, and which pad the
    user has selected as well.

--- assets/scripts/drumEvents.js
+++ assets/scripts/drumEvents.js
@@ -17,3 +17,41 @@ const addDrumHandlers = () => {
   width: 40,
   height: 30,
   });
+
+  //Trigger pad cannot be selected when no instruments are selected.
+  $(function() {
+    $('.pad').attr('disabled', true);
+  });
+
+  //find the correct user selected drum type
+    $('.drum.button').click(function() {
+      if ($(this).hasClass('kick')) {
+        currentDrum = 'kick';
+      }
+      if ($(this).hasClass('snare')) {
+        currentDrum = 'snare';
+      }
+      if ($(this).hasClass('hat-close')) {
+        currentDrum = 'hat-close';
+      }
+      if ($(this).hasClass('hat-open')) {
+        currentDrum = 'hat-open';
+      }
+      if ($(this).hasClass('clap')) {
+        currentDrum = 'clap';
+      }
+      drumPatterns.userSelectDrum(currentDrum);
+      $('.pad').attr('disabled', false);
+  });
+
+  //find the correct user selected trigger pad
+ $('.pad').click(function() {
+  let currentGrid = $(this).data('grid');
+  drumPatterns.padIndex(currentGrid);
+});
+};
+
+
+module.exports = {
+  addDrumHandlers,
+};
