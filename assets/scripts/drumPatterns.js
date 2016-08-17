5427ba26504117ca5bb9e624160c1214c15c2cc0
alex-braun <braunacb@gmail.com>
2016-08-17 11:18:46 -0400

    Placed arrays into beat object.
    Eliminated many redundant if/else statements!

--- assets/scripts/drumPatterns.js
+++ assets/scripts/drumPatterns.js
@@ -12,101 +12,47 @@ const knobJQuery = require('./jquery.knob.min');
 
 const triggerGrid = ['pad 1', 'pad 2', 'pad 3', 'pad 4', 'pad 5', 'pad 6', 'pad 7', 'pad 8'];
 
+const beat = {
+  kick: ['','','','','','','',''],
+  snare: ['','','','','','','',''],
+  hatClose: ['','','','','','','',''],
+  hatOpen: ['','','','','','','',''],
+  clap: ['','','','','','','',''],
+};
 
-let kickPattern = ['','','','','','','',''];
-let snarePattern = ['','','','','','','',''];
-let hatClosePattern = ['','','','','','','',''];
-let hatOpenPattern = ['','','','','','','',''];
-let clapPattern = ['','','','','','','',''];
 let drum;
-// let currentPattern;
 
-//This turns on indicators according to the arrays.
+
 let mapPatternsToIndicators = function(inst) {
-  if (inst === 'kick') {
-    for (let i = 0; i < triggerGrid.length; i++) {
-      let selected;
-      if (kickPattern[i] === true) {
-        selected = triggerGrid[i];
-        console.log(kickPattern);
-        $(".pad[data-grid ='"+selected+"']").closest('.pad').find('.trigger.indicator')
-        .addClass('clicked');
-      }
-      else if(kickPattern[i] === ''){
-        selected = triggerGrid[i];
-        $(".pad[data-grid ='"+selected+"']").closest('.pad').find('.trigger.indicator')
-        .removeClass('clicked');
-      }
-    }
+  for (let i = 0; i < triggerGrid.length; i++) {
+    let selected;
+    if (beat[inst][i] === true) {
+    selected = triggerGrid[i];
+    $(".pad[data-grid ='"+selected+"']").closest('.pad').find('.trigger.indicator')
+    .addClass('clicked');
+  }
+  else {
+    selected = triggerGrid[i];
+    $(".pad[data-grid ='"+selected+"']").closest('.pad').find('.trigger.indicator')
+    .removeClass('clicked');
+  }
   }
 };
-  //   else if (inst === 'snare' && snarePattern[i] === true) {
-  //     $('.pad').attr('data-grid',triggerGrid[i]).find('.trigger.indicator')
-  //     .addClass('clicked');
-  //   }
-  //   else if (inst === 'hat-close' && hatClosePattern[i] === true) {
-  //     $('.pad').attr('data-grid',triggerGrid[i]).find('.trigger.indicator')
-  //     .addClass('clicked');
-  //   }
-  //   else if (inst === 'hat-open' && hatOpenPattern[i] === true) {
-  //     $('.pad').attr('data-grid',triggerGrid[i]).find('.trigger.indicator')
-  //     .addClass('clicked');
-  //   }
-  //   else if (inst === 'clap' && clapPattern[i] === true) {
-  //     $('.pad').attr('data-grid',triggerGrid[i]).find('.trigger.indicator')
-  //     .addClass('clicked');
-  //   } else {
-  //     $('.pad').attr('data-grid',triggerGrid[i]).find('.trigger.indicatior')
-  //     .removeClass('clicked');
-  //   }
-  // };
 
 
 let constructPattern = function(drum, index) {
   // console.log(drum);
   for (let i = 0; i < triggerGrid.length; i++) {
-    if (triggerGrid[i] === index && drum === 'kick') {
-      if (kickPattern[i] === '') {
-        kickPattern[i] = true;
-      } else {
-        kickPattern[i] = '';
+    if(triggerGrid[i] === index) {
+      if(beat[drum][i] === '') {
+        beat[drum][i] = true;
       }
-        console.log(kickPattern);
-
-  } else if (triggerGrid[i] === index && drum === 'snare') {
-      if (snarePattern[i] === '') {
-        snarePattern[i] = true;
-      } else {
-        snarePattern[i] = '';
-      }
-        console.log(snarePattern);
-
-  } else if (triggerGrid[i] === index && drum === 'hat-close') {
-      if (hatClosePattern[i] === '') {
-        hatClosePattern[i] = true;
-      } else {
-        hatClosePattern[i] = '';
-      }
-        console.log(hatClosePattern);
-
-  } else if (triggerGrid[i] === index && drum === 'hat-open') {
-      if (hatOpenPattern[i] === '') {
-        hatOpenPattern[i] = true;
-      } else {
-        hatOpenPattern[i] = '';
-      }
-        console.log(hatOpenPattern);
-
-  } else if (triggerGrid[i] === index && drum === 'clap') {
-      if (clapPattern[i] === '') {
-      clapPattern[i] = true;
-      } else {
-        clapPattern[i] = '';
-      }
-        console.log(clapPattern);
+      else {
+        beat[drum][i] = '';
       }
     }
-
+  }
+  // console.log(beat[drum]);
 };
 
 
@@ -118,42 +64,6 @@ let padIndex = function(grid) {
   constructPattern(drum, grid);
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let mapPatternsToIndicators = function() {
-//   for (let i = 0; i < triggerGrid.length; i++) {
-//     if (currentPattern[i] === true) {
-//       ('.pad').data(triggerGrid[i]).attr('class','clicked');
-//     }
-//   }
-// };
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = {
