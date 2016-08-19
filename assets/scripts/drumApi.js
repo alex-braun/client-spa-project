a861b3879e8752e27176b6a9cf5230237669ea9e
alex-braun <braunacb@gmail.com>
2016-08-19 10:09:37 -0400

    Created baseline ajax requests.  Not tested.

--- assets/scripts/drumApi.js
+++ assets/scripts/drumApi.js
@@ -1,11 +1,57 @@
 'use strict';
 const app = require('./app');
 
+const patternCreate = () => $.ajax({
+      url: app.api + '/patterns',
+      method: 'POST',
+      data: '',
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+      },
+    });
 
+
+  const patternShowAll = (data) => $.ajax({
+      url: app.api + '/games/',
+      method: 'GET',
+      data,
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+      },
+  });
+
+  const patternShow = (data) => $.ajax({
+      url: app.api + '/games/' + data.pattern.id,
+      method: 'GET',
+      data,
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+      },
+  });
+
+
+  const patternUpdate = function (gridLocation, player, over) {
+      return $.ajax({
+      url: app.api + '/games/' + app.pattern.id,
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + app.user.token,
+      },
+      data: {
+                "game": {
+                  "cell": {
+                    "index": gridLocation,
+                    "value": player,
+                  },
+                  "over": over,
+                }
+              },
+  });
+};
 
 module.exports = {
   patternCreate,
-  patternsShowAll,
+  patternShowAll,
   patternShow,
   patternUpdate,
 };
