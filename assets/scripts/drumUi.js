ebd1ba67ca2dd3cbda70126f5f772d578670312a
alex-braun <braunacb@gmail.com>
2016-08-19 09:59:17 -0400

    UI for drum crud responses.

---
+++ assets/scripts/drumUi.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const app = require('./app');
+const drumPatterns = require('./drumPatterns');
+
+
+
+
+module.exports = {
+  failure,
+  createSuccess,
+  showAllSuccess,
+  showPatternSuccess,
+};
