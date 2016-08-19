62abd0bd62caa5123c63e0f940ecaf4987a4694c
alex-braun <braunacb@gmail.com>
2016-08-19 13:16:37 -0400

    drumApi show all and show 1 completed.
    Beats are not showing, but they have not been made yet!

--- assets/scripts/drumApi.js
+++ assets/scripts/drumApi.js
@@ -1,18 +1,31 @@
 'use strict';
 const app = require('./app');
 
-const patternCreate = () => $.ajax({
-      url: app.api + '/patterns',
-      method: 'POST',
-      data: '',
-      headers: {
-        Authorization: 'Token token=' + app.user.token,
-      },
-    });
+// const patternCreate = () => $.ajax({
+//       url: app.api + '/patterns',
+//       method: 'POST',
+//       headers: {
+//         Authorization: 'Token token=' + app.user.token,
+//       },
+//       data: {
+//             "name":
+//             "user_id":
+//       });
+
+
+    // curl --include --request POST http://localhost:3000/patterns \
+    //   --header "Authorization: Token token=BAhJIiUwZWQ5M2RhYTVlZmYwOWMzOTExZDVmZWM5N2Y2ZGEzNgY6BkVG--1fbbfe83cd2c6d95f8c657abc8a079c524104281" \
+    //   --header "Content-Type: application/json" \
+    //   --data '{
+    //     "pattern": {
+    //       "name": "Rock",
+    //       "user_id": "2"
+    //     }
+    //   }'
 
 
   const patternShowAll = (data) => $.ajax({
-      url: app.api + '/games/',
+      url: app.api + '/patterns/',
       method: 'GET',
       data,
       headers: {
@@ -21,7 +34,7 @@ const patternCreate = () => $.ajax({
   });
 
   const patternShow = (data) => $.ajax({
-      url: app.api + '/games/' + data.pattern.id,
+      url: app.api + '/patterns/' + data.pattern.id,
       method: 'GET',
       data,
       headers: {
@@ -29,29 +42,29 @@ const patternCreate = () => $.ajax({
       },
   });
 
-
-  const patternUpdate = function (gridLocation, player, over) {
-      return $.ajax({
-      url: app.api + '/games/' + app.pattern.id,
-      method: 'PATCH',
-      headers: {
-        Authorization: 'Token token=' + app.user.token,
-      },
-      data: {
-                "game": {
-                  "cell": {
-                    "index": gridLocation,
-                    "value": player,
-                  },
-                  "over": over,
-                }
-              },
-  });
-};
+//updates the pattern after the "save" button
+//   const patternUpdate = function (gridLocation, player, over) {
+//       return $.ajax({
+//       url: app.api + '/patterns/' + app.pattern.id,
+//       method: 'PATCH',
+//       headers: {
+//         Authorization: 'Token token=' + app.user.token,
+//       },
+//       data: {
+//                 "game": {
+//                   "cell": {
+//                     "index": gridLocation,
+//                     "value": player,
+//                   },
+//                   "over": over,
+//                 }
+//               },
+//   });
+// };
 
 module.exports = {
-  patternCreate,
+  // patternCreate,
   patternShowAll,
   patternShow,
-  patternUpdate,
+  // patternUpdate,
 };
