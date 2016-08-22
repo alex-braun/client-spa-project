76728d77365fa5c01cf0d8846ce85fcd0385216a
alex-braun <braunacb@gmail.com>
2016-08-22 10:28:50 -0400

    Added api: 'https://drum-sequencer.herokuapp.com'
    Pushing for deployment.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  api: 'http://localhost:3000',
-  //  api: 'https://aqueous-atoll-85096.herokuapp.com',
+  // api: 'http://localhost:3000',
+  api: 'https://drum-sequencer.herokuapp.com'
 };
 
 module.exports = app;
