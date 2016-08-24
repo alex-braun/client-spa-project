d62fd607d9a00618aa5c498208a9bf1f29c83a51
alex-braun <braunacb@gmail.com>
2016-08-23 20:22:38 -0400

    Gave h2 append mess a class.
    Allows empty.append to properly clear contents and be used again
    with only clearing the text instead of emptying the whole container div.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -14,22 +14,23 @@ const signInSuccess = (data) => {
   user = data.user.id;
   console.log(app);
   $('.command-top').empty().append('<h2>Hello, user: ' + data.user.id + '</h2>').fadeIn(2000);
-  $('.command-bottom').empty().append('<h2>Ready to make some beats?</h2>').fadeIn(2000)
-  .delay(2000).fadeOut(1000);
+  $('.command-bottom').empty().append('<h2 class = "message">Ready to make some beats?</h2>');
+  $('.message').fadeIn(2000).delay(2000).fadeOut(1000);
   $('.crud').attr('disabled', false);
 };
 
 const signOutSuccess = () => {
   delete app.user;
   console.log(app);
-  $('.command-top').empty().append('<h2>User: ' + user + '<br>Sign out successful</h2>')
-  .fadeOut(2000);
+  $('.command-top').empty().append('<h2 class = "main">User: ' + user + '<br>Sign out successful</h2>');
+  $('.main').fadeOut(3000);
   $('.crud').attr('disabled', true);
 
 };
 
 const changePassSuccess = (data) => {
-    $('.command-bottom').empty().append('<h2>Password update successful</h2>').fadeOut(2000);
+    $('.command-bottom').empty().append('<h2 class = "message">Password update successful</h2>');
+    $('.message').fadeOut(3000);
   console.log(data);
 };
 
@@ -39,7 +40,8 @@ const failure = (error) => {
 
 const signInFailure = (error) => {
   console.log(error);
-  $('.command-top').empty().append('<h2>Error! Check your password or sign Up!</h2>').fadeOut(4000);
+  $('.command-top').empty().append('<h2 class = "main">Error! Check your password or sign Up!</h2>');
+  $('.main').fadeOut(4000);
 };
 
 
